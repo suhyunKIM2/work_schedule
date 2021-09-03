ccbce76ad063eee83cb062cec872d35846336f4e
suhyunKIM2 <87849293+suhyunKIM2@users.noreply.github.com>
2021-09-03 17:00:40 +0900

    나의근무스케쥴 _ 1. 추가하기 해당 특정 라인에만 추가 되게 수정하기 2. select 휴무 셀렉시 해당 셀렉박스빼고 감추기 진행해야함

---
+++ webapps/hr/attendance/js/calendar.js
@@ -0,0 +1,32 @@
+﻿// JavaScript Document
+$(function () {
+       
+        $.datepicker.setDefaults({
+        dateFormat: 'yy-mm-dd',
+        prevText: '이전 달',
+        nextText: '다음 달',
+        closeText: '닫기',
+        monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+        dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],	
+        dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+        showMonthAfterYear: true,
+        yearSuffix: '년',
+        showButtonPanel: true,
+        changeMonth: true,
+        changeYear: true, 
+    });
+     $("#datepicker").datepicker();
+         $("#datepicker").datepicker('setDate', new Date);
+        $("#datepicker2").datepicker();
+        $("#datepicker2").datepicker('setDate', new Date);
+        $("#datepicker3").datepicker();
+        $("#datepicker3").datepicker('setDate', new Date);
+        $("#datepicker4").datepicker();
+        $("#datepicker4").datepicker('setDate', new Date);
+        $("#datepicker5").datepicker();
+        $("#datepicker5").datepicker('setDate', new Date);
+        $("#datepicker6").datepicker();
+        $("#datepicker6").datepicker('setDate', new Date);
+    });
---
+++ webapps/js/main_v2.js
@@ -0,0 +1,99 @@
+﻿$(window).load(function() {
+	$("body").css('visibility','visible').animate({opacity:1},100);
+});
+$(document).ready(function(){
+var contentColHeight = $('.navbar').height();
+//var optionColHeight = $('.detail_area ').height();
+var num = contentColHeight+ 80;
+var box = $('.container').height();
+var windowbox = $(window).height();
+
+
+$(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 57) {
+		$('.navbar').css('padding-top','0').css('position','fixed');
+         
+    } else {
+		$(".navbar").css('padding-top','4%').css('position','static');/*★수정★*/
+    }
+});
+});
+
+
+
+$(function(){
+
+    var link = $('#navbar a.dot');
+    link.on('click',function(e){
+        
+        //href 속성을 통해, section id 타겟을 잡음
+        var target = $($(this).attr('href')); 
+        
+        //target section의 좌표를 통해 꼭대기로 이동
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        },100);
+        
+        //active 클래스 부여
+        $(this).addClass('active');
+
+        //앵커를 통해 이동할때, URL에 #id가 붙지 않도록 함.
+        e.preventDefault();
+    });
+    
+    $(window).on('scroll',function(){
+        findPosition();
+    });
+
+    function findPosition(){
+        $('section').each(function(){
+            if( ($(this).offset().top - $(window).scrollTop() ) < 20){
+                link.removeClass('active');
+                $('#navbar').find('[data-scroll="'+ $(this).attr('id') +'"]').addClass('active');
+            }
+        });
+    }
+
+    findPosition();
+});
+
+
+
+$(function(){
+	includeLayout();
+});
+function includeLayout(){
+	var includeArea= $('[data-include]');
+	var self, url;
+
+	$.each(includeArea, function(){
+		self= $(this);
+		url= self.data("include");
+		self.load(url, function(){
+			self.removeAttr("data-include");
+		});
+	});
+}
+
+
+
+/*
+window.addEventListener('load', function() {
+            var allElements = document.getElementsByTagName('*');
+            Array.prototype.forEach.call(allElements, function(el) {
+                var includePath = el.dataset.includePath;
+                if (includePath) {
+                    var xhttp = new XMLHttpRequest();
+                    xhttp.onreadystatechange = function () {
+                        if (this.readyState == 4 && this.status == 200) {
+                            el.outerHTML = this.responseText;
+                        }
+                    };
+                    xhttp.open('GET', includePath, true);
+                    xhttp.send();
+                }
+            });
+        });
+*/
+
+
