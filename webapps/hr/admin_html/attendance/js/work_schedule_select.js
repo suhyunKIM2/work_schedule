8dc236ed550df4527d8fddd7deb7a43c6a8ee2b3
suhyunKIM2 <87849293+suhyunKIM2@users.noreply.github.com>
2021-09-10 15:21:54 +0900

    어드민_ 근무스케쥴관리페이지 완료

--- webapps/hr/admin_html/attendance/js/work_schedule_select.js
+++ webapps/hr/admin_html/attendance/js/work_schedule_select.js
@@ -1,22 +1,4 @@
 ﻿
-$(document).on('click', '.schedule_table tr td ul li #add_action', function(){
-    var ul_list = $('.list_ul_work #ul_list');
-     var text_add = '지정근무지'
-    var btn_delete = 'X'
-     $(this).parent().find(ul_list).append("<li class='place_work appointed_work'><a href='javascript:wrapWindowByMask_designation();'>"+text_add+"</a><span class='btn-delete'>"+btn_delete+"</span></li>");
-});
-
-
-
-//Javascript
-$(document).on('click', '#ul_list li .btn-delete', function(e){
-    var idx = $('#ul_list li').index(this);
-    $(this).parent().eq(idx).remove();
-    event.stopPropagation();
-});
-
-
-
 $(function(){
 $(".list_name #selectBox_sun").on("change",function() {
 
@@ -29,7 +11,7 @@ $(".list_name #selectBox_sun").on("change",function() {
     }
     
     if($(this).val() == "option2") {
-         $(this).parent().parent().append("<li class='list_ul_work'><ul id='ul_list'><li class='place_work appointed_work'><a href='javascript:wrapWindowByMask_designation();'>지정근무지</a><span class='btn-delete'>X</span></li></ul><div class='add_action' id='add_action'>+ 추가하기</div></li>");
+         $(this).parent().parent().append("<li class='list_ul_work'><ul id='ul_list'><li class='place_work appointed_work'><a href='javascript:wrapWindowByMask_designation();'>지정근무지</a></li></ul></li>");
 
     }  else {
          $(this).parent().parent().find('.list_ul_work').remove();
@@ -59,7 +41,7 @@ $(".list_name #selectBox_mon").on("change",function() {
     }
     
     if($(this).val() == "option2") {
-         $(this).parent().parent().append("<li class='list_ul_work'><ul id='ul_list'><li class='place_work appointed_work'><a href='javascript:wrapWindowByMask_designation();'>지정근무지</a><span class='btn-delete'>X</span></li></ul><div class='add_action' id='add_action'>+ 추가하기</div></li>");
+         $(this).parent().parent().append("<li class='list_ul_work'><ul id='ul_list'><li class='place_work appointed_work'><a href='javascript:wrapWindowByMask_designation();'>지정근무지</a></li></ul></li>");
 
     }  else {
          $(this).parent().parent().find('.list_ul_work').remove();
@@ -89,7 +71,7 @@ $(".list_name #selectBox_tue").on("change",function() {
     }
     
     if($(this).val() == "option2") {
-         $(this).parent().parent().append("<li class='list_ul_work'><ul id='ul_list'><li class='place_work appointed_work'><a href='javascript:wrapWindowByMask_designation();'>지정근무지</a><span class='btn-delete'>X</span></li></ul><div class='add_action' id='add_action'>+ 추가하기</div></li>");
+         $(this).parent().parent().append("<li class='list_ul_work'><ul id='ul_list'><li class='place_work appointed_work'><a href='javascript:wrapWindowByMask_designation();'>지정근무지</a></li></ul></li>");
 
     }  else {
          $(this).parent().parent().find('.list_ul_work').remove();
@@ -119,7 +101,7 @@ $(".list_name #selectBox_wed").on("change",function() {
     }
     
     if($(this).val() == "option2") {
-         $(this).parent().parent().append("<li class='list_ul_work'><ul id='ul_list'><li class='place_work appointed_work'><a href='javascript:wrapWindowByMask_designation();'>지정근무지</a><span class='btn-delete'>X</span></li></ul><div class='add_action' id='add_action'>+ 추가하기</div></li>");
+         $(this).parent().parent().append("<li class='list_ul_work'><ul id='ul_list'><li class='place_work appointed_work'><a href='javascript:wrapWindowByMask_designation();'>지정근무지</a></li></ul></li>");
 
     }  else {
          $(this).parent().parent().find('.list_ul_work').remove();
@@ -149,7 +131,7 @@ $(".list_name #selectBox_thu").on("change",function() {
     }
     
     if($(this).val() == "option2") {
-         $(this).parent().parent().append("<li class='list_ul_work'><ul id='ul_list'><li class='place_work appointed_work'><a href='javascript:wrapWindowByMask_designation();'>지정근무지</a><span class='btn-delete'>X</span></li></ul><div class='add_action' id='add_action'>+ 추가하기</div></li>");
+         $(this).parent().parent().append("<li class='list_ul_work'><ul id='ul_list'><li class='place_work appointed_work'><a href='javascript:wrapWindowByMask_designation();'>지정근무지</a></li></ul></li>");
 
     }  else {
          $(this).parent().parent().find('.list_ul_work').remove();
@@ -179,7 +161,7 @@ $(".list_name #selectBox_fri").on("change",function() {
     }
     
     if($(this).val() == "option2") {
-         $(this).parent().parent().append("<li class='list_ul_work'><ul id='ul_list'><li class='place_work appointed_work'><a href='javascript:wrapWindowByMask_designation();'>지정근무지</a><span class='btn-delete'>X</span></li></ul><div class='add_action' id='add_action'>+ 추가하기</div></li>");
+         $(this).parent().parent().append("<li class='list_ul_work'><ul id='ul_list'><li class='place_work appointed_work'><a href='javascript:wrapWindowByMask_designation();'>지정근무지</a></li></ul></li>");
 
     }  else {
          $(this).parent().parent().find('.list_ul_work').remove();
@@ -209,7 +191,7 @@ $(".list_name #selectBox_sat").on("change",function() {
     }
     
     if($(this).val() == "option2") {
-         $(this).parent().parent().append("<li class='list_ul_work'><ul id='ul_list'><li class='place_work appointed_work'><a href='javascript:wrapWindowByMask_designation();'>지정근무지</a><span class='btn-delete'>X</span></li></ul><div class='add_action' id='add_action'>+ 추가하기</div></li>");
+         $(this).parent().parent().append("<li class='list_ul_work'><ul id='ul_list'><li class='place_work appointed_work'><a href='javascript:wrapWindowByMask_designation();'>지정근무지</a></li></ul></li>");
 
     }  else {
          $(this).parent().parent().find('.list_ul_work').remove();
@@ -228,51 +210,8 @@ $(".list_name #selectBox_sat").on("change",function() {
 });
 
 
-    
-$(function(){
-$(".#selectBox_Entry").on("change",function() {
 
-     
-    if($("#selectBox_Entry").val() == "basic_Entry") {
-         $('.appointed').css('display','none');
-         $('#address_basic_input').css('display','inline-block');
-         $('.Entry_btn').css('display','block');
 
-    }  else {
-         $('.appointed').css('display','table-row');
-         $('#address_basic_input').css('display','none');
-         $('.Entry_btn').css('display','none');
-    }
-    
-    if($("#selectBox_Entry").val() == "appointed_Entry") {
-         $('.appointed').css('display','table-row');
-         $('#address_input').css('display','inline-block');
-         $('.blue_pencil_btn').css('display','block');
-
-    }  else {
-         $('.appointed').css('display','none');
-         $('#address_input').css('display','none');
-         $('.blue_pencil_btn').css('display','none');
-    }
-    
-}) 
-});  
-
-$(function(){
-$("#list_Entry").on("change",function() {
-
-     
-    if($("#list_Entry").val() == "list") {
-         $('.Entry_btn').css('display','none');
-         $('.blue_pencil_btn').css('display','block');
-
-    }  else {
-         $('.Entry_btn').css('display','block');
-         $('.blue_pencil_btn').css('display','none');
-    }
-    
-}) 
-});
 
 function setValues() {
         var sh = document.getElementById("list_Entry");
