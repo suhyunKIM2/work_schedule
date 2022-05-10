15cd19e4beaa8a3185392c64e5481548f3ac5bca
suhyunKIM2 <87849293+suhyunKIM2@users.noreply.github.com>
2022-05-10 15:39:18 +0900

    테니스 아레나 아이콘 수정 적용

---
+++ webapps/admin/js/calendar.js
@@ -0,0 +1,23 @@
+﻿// JavaScript Document
+$(function () {
+        $("#datepicker").datepicker();
+        $("#datepicker2").datepicker();
+        $("#datepicker3").datepicker();
+        $("#datepicker4").datepicker();
+        $("#datepicker5").datepicker();
+        $("#datepicker6").datepicker();
+        $.datepicker.setDefaults({
+        dateFormat: 'yy-mm-dd',
+        prevText: '이전 달',
+        nextText: '다음 달',
+        closeText: '닫기',
+        monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+        dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],	
+        dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+        showMonthAfterYear: true,
+        yearSuffix: '년',
+        showButtonPanel: true,
+    });
+    });
---
+++ webapps/js/layer_popup.js
@@ -0,0 +1,42 @@
+﻿// JavaScript Document
+$(function () {
+  // 레이어 display none 상태
+  $(".layer_bg, .layer_wrap").hide();
+  //레이어팝업 위치 지정 function 만들기
+  function layer_position(){
+    var win_W = $(window).width();
+    var win_H = $(window).height();
+    $(".layer_wrap").css({'left':(win_W-500)/2, 'top':(win_H-500)/2});
+  };
+  //레이어팝업 open 상태 function 만들기
+  function layer_open(no){
+    $(".layer_wrap[layer="+no+"]").fadeIn();
+    $(".layer_bg").fadeIn();
+    layer_position();
+    //레이어 영역 외 바탕화면 클릭시 화면 닫기
+    $(".layer_bg").click(function (e) {
+      if(!$(".layer_wrap").has(e.target).length){
+        layer_close();
+      };
+    });
+  };
+  //레이어팝업 close 상태 function 만들기
+  function layer_close(){
+    $(".layer_wrap, .layer_bg").fadeOut();
+  };
+  //링크 클릭시 해당 레이어팝업 호출
+  $(".btn_layer").click(function () {
+    var no = $(this).attr("layer");
+    layer_open(no);
+  });
+  //닫기 버튼 클릭시 레이어 닫기
+  $(".btn_close").click(function () {
+    layer_close();
+  });
+  //반응형 대응 - 레이어 위치 잡기
+  $(window).resize(function () {
+    layer_position();
+  });
+})
+
+
